fa29b1ae30d9ecd60cc20f3ac95c073d11601be1
M-Shafay <shafay0099@gmail.com>
2024-10-08 11:39:48 +0500

    Admin done

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import Adminindex from './components/Adminindex';
+import Adminchart from './components/Adminchart';
+import Adminmaps from './components/Adminmaps';
+import Adminicon from './components/Adminicon';
+import Adminpageblank from './components/Adminpageblank';
+import Adminprofile from './components/Adminprofile';
+import Adminsignin from './components/Adminsignin';
+import Adminsignup from './components/Adminsignup';
+import Adminuibuttons from './components/Adminuibuttons';
+import Adminuicards from './components/Adminuicards';
+import Adminuiform from './components/Adminuiform';
+import Adminuitypography from './components/Adminuitypography';
+import Adminpro from './components/Adminpro';
+
+function App() {
+  return (
+    // <Adminicon/>
+    //<Adminpageblank/>
+    //<Adminprofile/>
+    //<Adminsignin/>
+    //<Adminsignup/>
+    //<Adminuibuttons/>
+    //<Adminuicards/>
+    //<Adminuiform/>
+    //<Adminuitypography/>
+    <Adminpro/>
+  );
+}
+
+export default App;
